94c374569ec0ff70dbedd032a9bdc81b9794eb62
Alejandro Palacio <alejandrooss94@gmail.com>
2023-09-08 15:10:03 -0500

    Se implementa el metodo POST

--- src/main/java/com/alejopp/todoapp/controller/TaskController.java
+++ src/main/java/com/alejopp/todoapp/controller/TaskController.java
@@ -1,8 +1,28 @@
 package com.alejopp.todoapp.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alejopp.todoapp.persistence.entity.Task;
+import com.alejopp.todoapp.service.TaskService;
+import com.alejopp.todoapp.service.dto.TaskInDTO;
+
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
     
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskInDTO) {
+        return this.taskService.createTask(taskInDTO);
+    }
+
 }
