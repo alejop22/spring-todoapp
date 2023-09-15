157c40dbdd7e4ecaae9231c2d93ca83a8501bca8
Alejandro Palacio <alejandrooss94@gmail.com>
2023-09-15 11:56:01 -0500

    Configuracion basica de seguridad

--- src/main/java/com/alejopp/todoapp/context/security/SecurityConfig.java
+++ src/main/java/com/alejopp/todoapp/context/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.alejopp.todoapp.context;
+package com.alejopp.todoapp.context.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
